928dd747cd6601d469792eb96ace4e69c9a55467
wwarriner <wwarriner@gmail.com>
2020-12-07 15:22:39 -0600

    added fabio crameri work need license before merging

---
+++ helpers/mc_input_parse.m
@@ -0,0 +1,17 @@
+function parsed = mc_input_parse(varargin)
+
+if nargin < 1
+    m = 256;
+end
+
+assert(isnumeric(m));
+assert(isscalar(m));
+assert(isreal(m));
+assert(isfinite(m));
+assert(mod(m, 1) == 0);
+assert(1 <= m);
+
+parsed.m = m;
+
+end
+
