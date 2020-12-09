d3d05989fdea5c63ee216a6f5c162c8142f53bab
wwarriner <wwarriner@gmail.com>
2020-12-09 14:31:15 -0600

    updated showcase to show shading effects on surfs in 3d better gray-color comparison

--- showcase_cmap.m
+++ showcase_cmap.m
@@ -4,53 +4,83 @@ if nargin < 1
     c = viridis;
 end
 
-[X, Y, Z] = peaks(100);
-
+% setup
 fh = figure();
 fh.MenuBar = "none";
 fh.ToolBar = "none";
 fh.DockControls = "off";
 fh.Color = [0.9 0.9 0.9];
-fh.Position(2) = 50;
-fh.Position(3:4) = [1280 960];
+fh.Position(1:2) = [50 50];
+fh.Position(3:4) = [1600 960];
 
-axh = subplot(2, 3, 1);
+% hi-res peaks
+[X, Y, Z] = peaks(200);
+
+axh = subplot(2, 4, 1);
 sh = surfc(axh, X, Y, Z);
-sh(1).EdgeColor = "interp";
+sh(1).EdgeColor = "none";
 sh(1).FaceColor = "interp";
-sh(1).FaceLighting = "gouraud";
+light(axh, "position", [0 0 5], "style", "local");
 view(axh, 3);
 colormap(axh, c);
 axis(axh, "square", "vis3d");
-axh.Title.String = "surfc(peaks(100)), view(1)";
+axh.Title.String = "surfc(peaks(200)), view(1)";
+axh.Toolbar.Visible = "on";
+axtoolbar(axh, "rotate");
 
-axh = subplot(2, 3, 2);
-sh = surf(axh, X, Y, Z);
-sh(1).EdgeColor = "interp";
-sh(1).FaceColor = "interp";
-sh(1).FaceLighting = "gouraud";
+axh = subplot(2, 4, 2);
+imagesc(axh, Z);
+axh.YDir = "normal";
 view(axh, 2);
 colormap(axh, c);
 axis(axh, "square");
-axh.Title.String = "surfc(peaks(100)), view(2)";
+axh.Title.String = "surfc(peaks(200)), view(2)";
+axh.Toolbar.Visible = "off";
 
-axh = subplot(2, 3, 3);
-Z = peaks(20);
-ih = imagesc(axh, Z);
+% lo-res peaks
+[X, Y, Z] = peaks(10);
+
+axh = subplot(2, 4, 5);
+sh = surfc(axh, X, Y, Z);
+sh(1).EdgeColor = "none";
+sh(1).FaceColor = "interp";
+light(axh, "position", [0 0 5], "style", "local");
+view(axh, 3);
+colormap(axh, c);
+axis(axh, "square", "vis3d");
+axh.Title.String = "surfc(peaks(20)), view(1)";
+axh.Toolbar.Visible = "on";
+axtoolbar(axh, "rotate");
+
+axh = subplot(2, 4, 6);
+imagesc(axh, Z);
 axh.YDir = "normal";
+view(axh, 2);
 colormap(axh, c);
 axis(axh, "square");
-axh.Title.String = "imagesc(peaks(20))";
+axh.Title.String = "surfc(peaks(20)), view(2)";
+axh.Toolbar.Visible = "off";
 
-axh = subplot(2, 3, 4);
+% test image
 im = imread("rice.png");
 im = imadjust(im);
-ih = imagesc(axh, im);
+
+axh = subplot(2, 4, 3);
+imshow(im);
+axis(axh, "square");
+axh.Title.String = "rice.png, grayscale";
+axh.Toolbar.Visible = "off";
+
+axh = subplot(2, 4, 4);
+imagesc(axh, im);
 colormap(axh, c);
 axis(axh, "square");
-axh.Title.String = "rice.png";
+axh.Box = "off";
+axh.Title.String = "rice.png, color";
+axh.Toolbar.Visible = "off";
 
-axh = subplot(2, 3, [5 6]);
+% sine ramp
+axh = subplot(2, 4, [7 8]);
 im = ramp_sine_test();
 ih = imagesc(axh, im);
 colormap(axh, c);
